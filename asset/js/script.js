8742bb43db8cdce88cb5dd5a5a5fe78680db0c37
nurjayakusuma <ahoydata@gmail.com>
2024-09-24 16:22:44 +0700

    24092024

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,11 +1,33 @@
-const container = document.querySelector('.card-container');
-const button = document.querySelectorAll('i');
-let firstCardWidth =  document.querySelector('.card-project').offsetWidth;
-console.log(button);
+// const container = document.querySelector('.card-container');
+// const button = document.querySelectorAll('i');
+// let firstCardWidth =  document.querySelector('.card-project').offsetWidth;
+// console.log(button);
 
-button.forEach(function(btn) {
-  btn.addEventListener('click', function(){
-    console.log(btn.id);
-    container.scrollLeft += btn.id === 'kiri' ? -firstCardWidth : firstCardWidth;
-  });
+// button.forEach(function(btn) {
+//   btn.addEventListener('click', function(){
+//     console.log(btn.id);
+//     container.scrollLeft += btn.id === 'kiri' ? -firstCardWidth : firstCardWidth;
+//   });
+// });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
