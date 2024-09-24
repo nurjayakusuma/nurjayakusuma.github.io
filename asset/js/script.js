5da6e9859e192848e457a2be19a1f13f939450db
nurjayakusuma <ahoydata@gmail.com>
2024-09-24 16:32:34 +0700

    -

--- asset/js/script.js
+++ asset/js/script.js
@@ -14,7 +14,12 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-
+  speed: 500,
+  autoplay: {
+    delay: 5000
+  },
+  
+  slidesPerView: 4,
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
