91c63acc4e8ec46ce86ca7505665dca053c0fee5
nurjayakusuma <ahoydata@gmail.com>
2024-09-25 11:29:29 +0700

    menambah coment pada javascript

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,15 +1,4 @@
-// const container = document.querySelector('.card-container');
-// const button = document.querySelectorAll('i');
-// let firstCardWidth =  document.querySelector('.card-project').offsetWidth;
-// console.log(button);
-
-// button.forEach(function(btn) {
-//   btn.addEventListener('click', function(){
-//     console.log(btn.id);
-//     container.scrollLeft += btn.id === 'kiri' ? -firstCardWidth : firstCardWidth;
-//   });
-// });
-
+// swiperJS
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
